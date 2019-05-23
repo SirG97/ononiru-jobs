8933a7262e8639f586bcf9d081bc639e27ae6e6b
ligerphp <johnsonmessilo19@gmail.com>
2019-05-23 10:53:00 -0700

    fix apply ui

--- js/jobApply.js
+++ js/jobApply.js
@@ -1,15 +1,15 @@
 function jobApplyInit(id,company_id){
     let c_id = company_id;
     let jb_id = id;
-    //check if user is logged in
-   let l_user_id = window.localStorage.getItem('user_id');
-    if(!l_user_id){
-        location.href = `login.php?prev=${location.href}`;
-    }
+
+    document.getElementById('okbtn').innerHTML = 'Applying...';
+    document.getElementById('okbtn').disabled = 'true';
+
+    document.getElementById('canclebtn').style.display = 'none';
+
      data = {
         job_id:jb_id,
         company_id:c_id,
-        user_id:l_user_id
     }
 
     $.ajax({
@@ -18,8 +18,42 @@ function jobApplyInit(id,company_id){
         data,
     }).done(data => {
         console.log(data);
+        if(data.status_code == 200 && data.status == 'success'){
+
+
+    document.getElementById('successMsg').innerHTML = `
+    <div class='ui success message floating'>
+    <i class='close icon'>
+            </i>
+            <div class='header'>
+            Your application was successful!
+            </div>
+            </div>`;
+    document.getElementById('okbtn').innerHTML = 'Great,please wait...';
+    setTimeout(() => {
+        document.getElementById('canclebtn').click();
+        document.getElementById('job_status').innerHTML =  `
+        <div class="ui green horizontal label">Applied</div>
+        `;
+    },1500)
+
+
+        }else if(data.status == 'failed' && data.status_code == 409 && data.message == "You cannot apply for this job a second time"){
+        
+            document.getElementById('okbtn').innerHTML = 'Apply';
+            document.getElementById('canclebtn').style.display = 'inline';
+            document.getElementById('alertdesc').innerHTML = data.message;
+        
+        }
+
+
     }).fail(err => {
         console.log(err)
     })
 
 }
+
+
+$('.message .close').on('click',function(){
+$(this).closest('.message').transition('fade');
+});
